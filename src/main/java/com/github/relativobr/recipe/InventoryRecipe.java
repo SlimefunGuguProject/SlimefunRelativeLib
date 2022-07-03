e49344a58b7ade12c41b8efd5d9307bcf6b32ca4
Ricardo <ricardo.relativo@gmail.com>
2022-07-02 23:36:51 -0300

    fix InventoryRecipe

--- src/main/java/com/github/relativobr/recipe/InventoryRecipe.java
+++ src/main/java/com/github/relativobr/recipe/InventoryRecipe.java
@@ -13,9 +13,9 @@ public final class InventoryRecipe {
       18, 19, 20, 21, 22, 23, 24, 25, 26,
       27, 28, 29, 30, 31, 32, 33, 34, 35,
       36, 37, 38, 39, 40, 41, 42, 43, 44,
-      45, 46, 47, 48, 49, 50, 51, 52
+      45, 46, 47, 48, 49, 50, 51, 52, 53
   };
-  public static final int[] NO_INV = {53};
+  public static final int[] NO_INV = {};
 
   //template chest
   //0,   1,  2,  3,  4,  5,  6,  7,  8
@@ -30,16 +30,16 @@ public final class InventoryRecipe {
   public static final int[] SIMPLE_BORDER = {
       0, 1, 2, 3, 4, 5, 6, 7, 8,
       12, 13, 14,
-      22, 23,
+      21, 23,
       30, 31, 32,
       36, 37, 38, 39, 40, 41, 42, 43, 44,
       45, 46, 47, 48, 49, 50, 51, 52, 53
   };
-  public static final int SIMPLE_STATUS_SLOT = 21;
+  public static final int SIMPLE_STATUS_SLOT = 22;
   public static final int[] SIMPLE_INPUT = {19};
   public static final int[] SIMPLE_INPUT_BORDER = {
       9, 10, 11,
-      18, 20,
+      18,     20,
       27, 28, 29
   };
   public static final int[] SIMPLE_OUTPUT = {25};
