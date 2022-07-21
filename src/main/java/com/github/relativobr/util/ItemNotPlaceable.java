7d84bece0d10dad63b4981a7b5251dc3a7203d74
Ricardo <ricardo.relativo@gmail.com>
2022-07-21 08:11:44 -0300

    v1.4.9 Fix ItemNotPlaceable

--- src/main/java/com/github/relativobr/util/ItemNotPlaceable.java
+++ src/main/java/com/github/relativobr/util/ItemNotPlaceable.java
@@ -1,10 +1,16 @@
 package com.github.relativobr.util;
 
+import io.github.thebusybiscuit.slimefun4.api.events.BlockPlacerPlaceEvent;
+import io.github.thebusybiscuit.slimefun4.api.events.PlayerRightClickEvent;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.attributes.NotPlaceable;
+import io.github.thebusybiscuit.slimefun4.core.handlers.BlockPlaceHandler;
+import io.github.thebusybiscuit.slimefun4.core.handlers.BlockUseHandler;
+import javax.annotation.Nonnull;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 
 public final class ItemNotPlaceable extends SlimefunItem implements NotPlaceable {
@@ -12,5 +18,26 @@ public final class ItemNotPlaceable extends SlimefunItem implements NotPlaceable
   public ItemNotPlaceable(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType,
       ItemStack[] recipe) {
     super(itemGroup, item, recipeType, recipe);
+    addItemHandler(onPlace(), onRightClick());
+  }
+
+  @Nonnull
+  private BlockPlaceHandler onPlace() {
+    return new BlockPlaceHandler(false) {
+      @Override
+      public void onPlayerPlace(BlockPlaceEvent e) {
+        e.setBuild(false);
+        e.setCancelled(true);
+      }
+      @Override
+      public void onBlockPlacerPlace(BlockPlacerPlaceEvent e) {
+        e.setCancelled(true);
+      }
+    };
+  }
+
+  @Nonnull
+  public BlockUseHandler onRightClick() {
+    return PlayerRightClickEvent::cancel;
   }
 }
